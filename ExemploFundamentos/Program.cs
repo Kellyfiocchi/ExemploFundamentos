51cf7d4c0314eeceec0d9d347b3a79e852238fba
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2023-10-11 18:07:38 -0300

    update

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,14 +1,6 @@
 ﻿using ExemploFundamentos.Common.Models;
 
 
-Pessoa pessoa1 = new Pessoa()
- {   Nome = "Kelly ",
-     Sobrenome = "Fiochi",
-      Idade = 34,
-      Profissao = "QA",
-    Hobby = "Tecnologia"
-  };
- pessoa1.Apresentar();
 
 
 
@@ -17,15 +9,15 @@ Pessoa pessoa1 = new Pessoa()
 
 
 
-
-
-
-
-
-
-
-
-
+// Pessoa pessoa1 = new Pessoa()
+//  {   
+//        Nome = "Kelly ",
+//      Sobrenome = "Fiochi",
+//       Idade = 34,
+//       Profissao = "QA",
+//     Hobby = "Tecnologia"
+//   };
+//  pessoa1.Apresentar();
 
 
 // Pessoa pessoa2 = new Pessoa()
@@ -404,4 +396,112 @@ Pessoa pessoa1 = new Pessoa()
 //  }
 
 
+
+
+
+
+
+
+
+
     
+// int[] arrayInteiros = new int[4];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
+
+
+// int [] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+
+// //Array.Resize(ref arrayInteiros,  arrayInteiros.Length * 2);
+
+// // Percorrendo o array com o For
+// Console.WriteLine("Percorrendo o array com o For");
+//  for(int contador =  0; contador < arrayInteiros.Length; contador++)
+//  {
+//      Console.WriteLine($"Posição N° {contador}: {arrayInteiros[contador]}");
+//  }
+
+
+
+
+
+
+
+// // Percorrendo o array com o Foreach
+// Console.WriteLine("Percorrendo o array com o Foreach");   
+// int contadorForeach = 0;
+// foreach (int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição N° {contadorForeach} {valor}");
+//     contadorForeach++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+List<string> listaString = new List<string>();
+
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("RJ");
+
+
+
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Remove("MG");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+
+
+// Console.WriteLine("Percorrendo a lista com o For");
+// for(int contador = 0; contador < listaString.Count; contador++)
+// {
+//     Console.WriteLine($"Posição N° {contador}: {listaString[contador]}");
+// }
+
+
+// Console.WriteLine("Percorrendo a lista com o Foreach");
+// int contadorForeach = 0;
+// foreach (string item in listaString)
+// {
+//     Console.WriteLine($"Posição N° {contadorForeach} {item}");
+//     contadorForeach++;
+// }
+
+
